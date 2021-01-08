647e583ec077912c37f9cbe41c511f7c49d69f68
Oleg Kulevets <olegkulevetsdev@gmail.com>
2021-01-08 17:24:43 +0200

    test55

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php get_header(); ?>
+<?php
+
+
+$test = 'hghgh';
+
+echo 'succcess';
+
+if ( $test ){
+    echo 'succcess5555555555555';
+
+}
+
+
+exit;
+
+
+
+get_header(); ?>
 <main class="clearfix">
   <div class="container">
     <h2 class="header-banner__sub-title"><?php the_title(); ?></h2>
